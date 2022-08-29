84967dae4b8cfa7883e5f80a93f0c6af5f2d3a06
Eddy G <eddy.spambari@gmail.com>
2022-08-29 06:23:43 -0700

    finishing later teehee

---
+++ main.js
@@ -0,0 +1,17 @@
+let output = 0; //current screen value
+
+function add (a, b) {
+    output = a + b;
+}
+
+function subtract (a, b) {
+    output = a - b;
+}
+
+function multiply (a, b) {
+    output = a * b;
+}
+
+function divide (a, b) {
+    output = a / b;
+}
